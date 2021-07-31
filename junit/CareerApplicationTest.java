e2fd53b11c8b0bf86f285da4681f11e0c97fa54a
Darren <Darren@DESKTOP-6RL7FGA>
2021-07-31 16:41:00 +0800

    These are the new items for our project, team members can improve on it.

---
+++ junit/CareerApplicationTest.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * Darren, 31 Jul 2021 11:15:04 am
+ */
+
+/**
+ * @author Darren
+ *
+ */
+public class CareerApplicationTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/Subject.java
@@ -0,0 +1,15 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * Darren, 31 Jul 2021 11:45:57 am
+ */
+
+/**
+ * @author Darren
+ *
+ */
+public class Subject {
+
+}
