ee0ef40c00dbc49c1844148c62dae50dd9ea24ba
gohjo <gohjo@LAPTOP-2KS16TG8.lan>
2021-08-03 15:06:38 +0800

    helphelphelp

---
+++ src/helpme.java
@@ -0,0 +1,12 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * gohjo, 3 Aug 2021 3:06:08 pm
+ */
+
+public class helpme {
+
+}
