d675abed4c078b17cbc34c7ef100b2fa43d62d1b
gohjo <gohjo@LAPTOP-2KS16TG8.lan>
2021-08-10 15:37:13 +0800

    test

--- src/CareerApplication.java
+++ src/CareerApplication.java
@@ -71,7 +71,7 @@ public class CareerApplication {
 				int sub_option = -1;
 				while (sub_option != 4) {
 					SubMenuCareer();
-					sub_option = Helper.readInt("Enter Choice > ");
+					sub_option = Helper.readInt("Enter Choice > ");//test
 
 					if (sub_option == 1) {
 						addCareer();
